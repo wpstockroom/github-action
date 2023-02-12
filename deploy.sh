a4999140916a4870b7ba95550966bc286fc38b4f
Jan Willem Oostendorp <webmaster@janw.me>
2023-02-12 15:50:06 +0100

    Prepared the action to actually function as a GH-action.

--- deploy.sh
+++ deploy.sh
@@ -3,51 +3,55 @@
 # Directory to self https://stackoverflow.com/a/246128/933065
 SELF_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-if [[ -z "$WEBSITE_URL" ]]; then
-  echo "Set the WEBSITE_URL secret"
+# make sure the github-action has the required packages.
+composer install
+
+if [[ -z "${STOCKROOM_URL}" ]]; then
+  echo "Set the STOCKROOM_URL in your github secret"
   exit 1
 fi
-if [[ -z "$WEBSITE_USER" ]]; then
-  echo "Set the WEBSITE_USER secret"
+if [[ -z "${STOCKROOM_USER}" ]]; then
+  echo "Set the STOCKROOM_USER in your github secret"
   exit 1
 fi
-if [[ -z "$WEBSITE_PASS" ]]; then
-  echo "Set the WEBSITE_PASS secret"
+if [[ -z "${STOCKROOM_PASS}" ]]; then
+  echo "Set the STOCKROOM_PASS in your github secret"
   exit 1
 fi
 
 # Allow some ENV variables to be customized
-if [[ -z "$SLUG" ]]; then
-  SLUG=${GITHUB_REPOSITORY#*/}
+if [[ -z "${SLUG}" ]]; then
+  SLUG=$(basename $(pwd))
 fi
-echo "SLUG is $SLUG"
 
-if [[ -z "$BUILD_DIR" ]]; then
-  BUILD_DIR=./
+if [[ -z "${BUILD_DIR}" ]]; then
+  BUILD_DIR=$(pwd)
 fi
 BUILD_DIR=${BUILD_DIR%/} # Strip trailing slash, always.
 
-echo "BUILD_DIR is $BUILD_DIR"
 
-# Determine version
-if [[ -f "$BUILD_DIR/style.css" ]]; then
-  VERSION=$(grep  'Version:.*' "$BUILD_DIR/style.css" | sed -E "s/.* ([.0-9])/\\1/")
+echo "BUILD_DIR is ${BUILD_DIR}"
+echo "SLUG is ${SLUG}"
+
+# Determine the version.
+if [[ -f "${BUILD_DIR}/style.css" ]]; then
+  VERSION=$(grep  'Version:.*' "${BUILD_DIR}/style.css" | sed -E "s/.* ([.0-9])/\\1/")
   echo "Theme version is ${VERSION}"
 else
-  VERSION=$(grep  'Version:.*' "$BUILD_DIR/$SLUG.php" | sed -E "s/.* ([.0-9])/\\1/")
+  VERSION=$(grep  'Version:.*' "${BUILD_DIR}/${SLUG}.php" | sed -E "s/.* ([.0-9])/\\1/")
   echo "Plugin version is ${VERSION}"
 fi
-if [[ -z "$VERSION" ]]; then
-  echo "Cannot determine version. It should be in ${SLUG}.php for plugins of style.css for themes."
+if [[ -z "${VERSION}" ]]; then
+  echo "Cannot determine version. It should be in ${SLUG}.php for plugins or style.css for themes."
   exit 1
 fi
 
 # Check for readme file.
-if [[ ! -f "$BUILD_DIR/readme.txt" ]]; then
+if [[ ! -f "${BUILD_DIR}/readme.txt" ]]; then
   echo "Cannot find readme.txt file. It is required."
   exit 1
 fi
-README_FILE="$BUILD_DIR/readme.txt"
+README_FILE="${BUILD_DIR}/readme.txt"
 
 # Full path to the zip file.
 ZIP_FILE="${BUILD_DIR}/${SLUG}.${VERSION}.zip"
@@ -82,6 +86,4 @@ zip -r -q ${ZIP_FILE} ./$( basename ${BUILD_DIR}) ${ZIP_EXCLUDES}
 set +o noglob # the `*` at the end of directories kept expanding.
 echo "Created zip file in ${ZIP_FILE}"
 
-composer install
-
-$SELF_PATH/deploy.php "$WEBSITE_URL" "$WEBSITE_USER" "$WEBSITE_PASS" "${VERSION}" -s "${SLUG}" -z "${ZIP_FILE}" -r "${README_FILE}"
+$SELF_PATH/deploy.php "${STOCKROOM_URL}" "${STOCKROOM_USER}" "${STOCKROOM_PASS}" "${VERSION}" -s "${SLUG}" -z "${ZIP_FILE}" -r "${README_FILE}"
---
+++ readme.md
@@ -0,0 +1,49 @@
+# WP Stockroom Github action
+
+A Github action to create and upload your selfhosted WordPress themes or plugins.
+
+## Usage
+
+To use this action create a file like `.github/workflows/deployment.yml`.
+The following
+
+```yaml
+name: Deploy to self hosted WP Stockroom
+on:
+  release:
+    types: [published]
+jobs:
+  release:
+    name: Deploy new Release
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@master
+      - name: Build # Remove or modify this step as needed
+        run: |
+          npm install
+          npm run build
+          composer install
+      - name: Plugin Deploy on Stockroom
+        uses: wpstockroom/github-action@stable
+        env:
+          STOCKROOM_URL: ${{ secrets.STOCKROOM_URL }}   # Required, the url to the Stockroom ie. https://wpstockroom.com  
+          STOCKROOM_USER: ${{ secrets.STOCKROOM_USER }} # Required, an existing WP user on the STOCKROOM_URL site. Should have editor permissions.
+          STOCKROOM_PASS: ${{ secrets.STOCKROOM_PASS }} # Required, an application password, please use separate passwords per theme/plugin.
+          BUILD_DIR: the-actual_dir                     # optional, when the actual theme/plugin not in the repo root, but in a subdirectory. 
+          SLUG: my-super-cool-plugin                    # optional, remove if GitHub repo name matches WP Stockroom slug, including capitalization
+```
+
+Add the following [Github secrets](https://docs.github.com/en/actions/security-guides/encrypted-secrets) to the repository.
+
+ - `STOCKROOM_URL` **Required**, the url to the Stockroom ie. https://wpstockroom.com  
+ - `STOCKROOM_USER` **Required**, an existing WP user on the STOCKROOM_URL site. Should have editor permissions.
+ - `STOCKROOM_PASS` **Required**, an application password, please use separate passwords per theme/plugin.
+ - `BUILD_DIR` _optional_, when the actual theme/plugin not in the repo root, but in a subdirectory. 
+ - `SLUG` _optional_, remove if GitHub repo name matches WP Stockroom slug, including capitalization
+
+## Excluding files
+
+You can specify files and folders in `.distignore`.  
+These are typically files that are only used during development, and not needed for the end user.  
+Like `.gitignore`, `node_modules`, `package.json` or `phpcs.xml` Here is decent example of a [.distignore](https://github.com/wpstockroom/wp-stockroom/blob/main/.distignore).
+
